3aa992d36504e3aa5c8b4838ba3c01d5bd7e7cc4
Gohil-Parikshitsinh <parikshitsinhgohil03@gmail.com>
2025-02-17 20:10:19 +0530

    Version 2.3.1

---
+++ App/course.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from PIL import Image, ImageTk
+class RMS:
+    def __init__(self,root):
+        self.root = root
+        self.root.title("Student Result Management System")
+        self.root.geometry("1350x700+0+0")
+        self.root.config(bg='white')
